7abc5c3ade0f613be403ec38ada321621724f4d8
Danilo Vasconcelos <53535711+DaniloVasconcelosBM@users.noreply.github.com>
2023-05-02 07:31:39 -0300

    feed

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,7 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import {RespostaPadraoMsg} from "../../types/RespostaPadraoMsg";
 import {validarTokenJWT} from "../../middlewares/validarTokenJwt";
-const usuarioEndPoint = (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json('Usuario Autenticado com sucesso');
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { UsuarioModel } from "../../models/UsuarioModel";
+
+const usuarioEndPoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+    try {
+        const {userId} = req?.query
+        const usuario = await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+    } catch (e) {
+        console.log(e);
+    } 
+
+    return res.status(400).json({erro: "não foi possivel obter dados do usuario"})
 }
 
-export default validarTokenJWT(usuarioEndPoint); //////////// funciona colocando o token manualmente!!!!!!!!!!!!!!!!!!!!!!!
+export default validarTokenJWT(conectarMongoDB(usuarioEndPoint)); 
